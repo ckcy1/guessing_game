87e3303c5881ea39541f9ff903f18c0591eddcdc
andy <ckcy1@qq.com>
2022-10-12 21:44:51 +0800

    初稿

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("猜数！!");
     println!("猜测一个数");
@@ -9,4 +10,10 @@ fn main() {
     let mut guess=String::new();
     io::stdin().read_line(&mut guess).expect("无法读取行");
     println!("你猜测的数是：{}",guess);
+    let guess:u32=guess.trim().parse().expect("type a number");
+    match guess.cmp(&sjs) {
+        Ordering::Less=>println!("太小了"),
+        Ordering::Greater=>println!("太大了"),
+        Ordering::Equal=>println!("你赢了"),
+    }
 }
